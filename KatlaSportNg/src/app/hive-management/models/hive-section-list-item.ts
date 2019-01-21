80729c307ef128f7e9d4ad0c193aa16cbca852bd
Andrey Golub <golubandrey31@gmail.com>
2019-01-21 17:51:12 +0300

    Add HiveSectionListItem fields to the front-end model. Add implementation of getHiveSections in  hive.service and ngOnInit in HiveSectionListComponent. Change HiveSectionListComponent page layout.

--- KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
+++ KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
@@ -1,4 +1,8 @@
 export class HiveSectionListItem {
     constructor(
+        public id: number,
+        public code: string,
+        public name:string,
+        public idDeleted:boolean
     ) { }
 }
