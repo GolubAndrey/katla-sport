fd5692273f5f0ed54847892b80f9f9623b009a1c
Andrey Golub <golubandrey31@gmail.com>
2019-01-26 21:35:16 +0300

    Add add, update and delete methods in HiveSectionService. Add fields to HiveSection model. Edited HiveSectionFormComponent page layout. Add Back, Save, Delete, Undelete and Purge buttons. Add application routing for HiveSectionFormComponent.

--- KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HiveSection } from '../models/hive-section'
+import { HiveSectionService} from '../services/hive-section.service'
 
 @Component({
   selector: 'app-hive-section-form',
@@ -7,8 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HiveSectionFormComponent implements OnInit {
 
-  constructor() { }
+  hiveSection = new HiveSection(0, "", "", 0, false, "");
+  existed = false;
+  hiveId: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private hiveSectionService: HiveSectionService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(p => {
+      this.hiveId = p['hiveId'];
+      if (p['id'] === undefined) return;
+      this.hiveSectionService.getHiveSection(p['id']).subscribe(s => this.hiveSection = s);
+      this.existed = true;
+    })
+  }
+
+  navigateToSections() {
+    if (this.hiveId === undefined){
+      this.hiveId = this.hiveSection.storeHiveId;
+    }
+    this.router.navigate([`/hive/${this.hiveId}/sections`]);
+  }
+
+  onCancel() {
+    this.navigateToSections();
+  }
+
+  onSubmit(){
+    this.hiveSection.storeHiveId = this.hiveId;
+    if (this.existed){
+      this.hiveSectionService.updateHiveSection(this.hiveSection).subscribe(() => this.navigateToSections());
+    }
+    else{
+      this.hiveSectionService.addHiveSection(this.hiveSection).subscribe(() => this.navigateToSections());
+    }
+  }
+
+  onDelete(){
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, true).subscribe(() => this.hiveSection.isDeleted = true);
+  }
+
+  onUndelete(){
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id,false).subscribe(()=>this.hiveSection.isDeleted = false);
+  }
+
+  onPurge(){
+    this.hiveSectionService.deleteHiveSection(this.hiveSection.id).subscribe(() => this.navigateToSections());
   }
 }
