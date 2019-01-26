2cdcc818bc9d7b5d421491e840b34185721c913a
Andrey Golub <golubandrey31@gmail.com>
2019-01-26 11:50:49 +0300

    Add UpdateHiveSectionRequest and UpdateHiveSectionRequestValidator classes with register UpdateHiveSectionRequest. Add add, update and delete methods for hive sections in section controller and service.

--- KatlaSport.DataAccess/Migrations/201901171356421_AddCategoryDescription.cs
+++ KatlaSport.DataAccess/Migrations/201901171356421_AddCategoryDescription.cs
@@ -6,16 +6,12 @@ namespace KatlaSport.DataAccess.Migrations
     {
         public override void Up()
         {
-            DropColumn("dbo.catalogue_products", "product_description");
-            DropColumn("dbo.catalogue_products", "product_manufacturer_code");
-            DropColumn("dbo.catalogue_products", "product_price");
+            AddColumn("dbo.product_categories", "category_description", c => c.String(maxLength: 300));
         }
 
         public override void Down()
         {
-            AddColumn("dbo.catalogue_products", "product_price", c => c.Decimal(precision: 18, scale: 2));
-            AddColumn("dbo.catalogue_products", "product_manufacturer_code", c => c.String(maxLength: 10));
-            AddColumn("dbo.catalogue_products", "product_description", c => c.String(maxLength: 300));
+            DropColumn("dbo.product_categories", "category_description");
         }
     }
 }
